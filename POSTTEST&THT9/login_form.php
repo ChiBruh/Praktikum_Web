e386a0e55e467f3635a5c1c94126ca4193981f49
ChiBruh <ichiroalbert@gmail.com>
2023-12-14 11:05:53 +0700

    Push POSTTEST & THT 9 ke MODUL-9

---
+++ POSTTEST&THT9/login_form.php
@@ -0,0 +1,5 @@
+<form action="login.php" method="post">
+    <label for="username">Username:</label>
+    <input type="text" name="username" required>
+    <button type="submit">Login</button>
+</form>
